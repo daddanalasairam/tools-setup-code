e6cb5ae905c419e4c7696b3f7c0322dbabcc50ec
daddanalasairam <sairamdaddanala11@gmail.com>
2026-04-27 13:07:24 +0530

    Commit

--- state.tf
+++ state.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket"
     key            = "tools/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
   }
 }
